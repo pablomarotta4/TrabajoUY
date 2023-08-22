cfe6cf7f867a50eba5045b15f31239f187199ba7
joaquin.cabrera <joaquin.cabrera.pastor@fing.edu.uy>
2023-08-21 21:52:20 -0300

    initial visual

--- src/presentacion/CrearOfertaLaboral.java
+++ src/presentacion/CrearOfertaLaboral.java
@@ -4,9 +4,18 @@ import javax.swing.JInternalFrame;
 
 import logica.interfaces.IControladorOferta;
 
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import javax.swing.JComboBox;
+import java.awt.Insets;
+import javax.swing.JCheckBox;
+
 public class CrearOfertaLaboral extends JInternalFrame{
 	
 	private IControladorOferta ctrlOferta;
+		
 	
 	public CrearOfertaLaboral(IControladorOferta cop) {
 		ctrlOferta = cop;	
@@ -16,7 +25,43 @@ public class CrearOfertaLaboral extends JInternalFrame{
         setMaximizable(true);
         setClosable(true);
         setTitle("Crear Oferta Laboral");
-        setBounds(10, 40, 360, 150);
+        setBounds(10, 40, 700, 700);
+        
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        gridBagLayout.columnWidths = new int[]{300, 300};
+        gridBagLayout.rowHeights = new int[]{50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50};
+        gridBagLayout.columnWeights = new double[]{0.0, 1.0};
+        gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+        getContentPane().setLayout(gridBagLayout);
+        
+        // Fila 1
+        JLabel lblEmpresa = new JLabel("Empresas");
+        GridBagConstraints gbc_lblEmpresa = new GridBagConstraints();
+        gbc_lblEmpresa.insets = new Insets(0, 0, 5, 5);
+        gbc_lblEmpresa.fill = GridBagConstraints.VERTICAL;
+        gbc_lblEmpresa.gridx = 0;
+        gbc_lblEmpresa.gridy = 0;
+        getContentPane().add(lblEmpresa, gbc_lblEmpresa);
+        
+        JComboBox comboBox = new JComboBox();
+        GridBagConstraints gbc_comboBox = new GridBagConstraints();
+        gbc_comboBox.insets = new Insets(0, 0, 5, 0);
+        gbc_comboBox.fill = GridBagConstraints.VERTICAL;
+        gbc_comboBox.gridx = 1;
+        gbc_comboBox.gridy = 0;
+        getContentPane().add(comboBox, gbc_comboBox);
+        
+        
+        JLabel lblTipoPublicacion = new JLabel("Tipos de publicacion");
+        GridBagConstraints gbc_lblTipoPublicacion = new GridBagConstraints();
+        gbc_lblTipoPublicacion.fill = GridBagConstraints.VERTICAL;
+        gbc_lblTipoPublicacion.insets = new Insets(0, 0, 5, 5);
+        gbc_lblTipoPublicacion.gridx = 0;
+        gbc_lblTipoPublicacion.gridy = 1;
+        getContentPane().add(lblTipoPublicacion, gbc_lblTipoPublicacion);
+        
+     
+
 	}
 	
 }
