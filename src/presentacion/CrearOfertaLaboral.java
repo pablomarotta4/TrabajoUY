2ba7f3618bcbddd68d8a427bffbbcc182530f8fc
joaquin.cabrera <joaquin.cabrera.pastor@fing.edu.uy>
2023-08-25 11:14:42 -0300

    fix layout ui

--- src/presentacion/CrearOfertaLaboral.java
+++ src/presentacion/CrearOfertaLaboral.java
@@ -27,9 +27,12 @@ import javax.swing.JCheckBox;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JPopupMenu;
+import javax.swing.JTextArea;
+
 import java.awt.Component;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.TextArea;
 
 public class CrearOfertaLaboral extends JInternalFrame{
 	
@@ -37,7 +40,6 @@ public class CrearOfertaLaboral extends JInternalFrame{
 	private IControladorUsuario ctrlUsuario;
 	private IControladorCompraTipo ctrlTipo;
 	private JTextField textNombreOferta;
-	private JTextField textDescripcion;
 	private JTextField textHorario;
 	private JTextField textRemuneracion;
 	private JTextField textCiudad;
@@ -65,40 +67,44 @@ public class CrearOfertaLaboral extends JInternalFrame{
         setMaximizable(true);
         setClosable(true);
         setTitle("Crear Oferta Laboral");
-        setBounds(10, 40, 700, 700);
+        setBounds(10, 40, 700, 600);
         
 
 
         GridBagLayout gridBagLayout = new GridBagLayout();
-        gridBagLayout.columnWidths = new int[]{300, 300};
-        gridBagLayout.rowHeights = new int[]{35, 35, 35, 35, 35, 30, 35, 35, 35, 35, 35, 35};
+        gridBagLayout.columnWidths = new int[]{140, 210, 200, 150};
+        gridBagLayout.rowHeights = new int[]{35, 35, 35, 35, 35, 30, 35, 35, 35, 35, 35, 35, 35};
         gridBagLayout.columnWeights = new double[]{0.0, 1.0};
         gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
         getContentPane().setLayout(gridBagLayout);
         
+
+
+   
         // ####################### Fila 1 ###############################
         JLabel lblEmpresa = new JLabel("Seleccionar empresa");
         GridBagConstraints gbc_lblEmpresa = new GridBagConstraints();
+        gbc_lblEmpresa.anchor = GridBagConstraints.WEST;
         gbc_lblEmpresa.insets = new Insets(0, 0, 5, 5);
         gbc_lblEmpresa.fill = GridBagConstraints.VERTICAL;
-        gbc_lblEmpresa.gridx = 0;
+        gbc_lblEmpresa.gridx = 1;
         gbc_lblEmpresa.gridy = 0;
         getContentPane().add(lblEmpresa, gbc_lblEmpresa);
         
         JComboBox<String> comboBoxEmpresa = new JComboBox<String>();
         GridBagConstraints gbc_comboBox = new GridBagConstraints();
-        gbc_comboBox.insets = new Insets(0, 0, 5, 0);
-        gbc_comboBox.fill = GridBagConstraints.VERTICAL;
-        gbc_comboBox.gridx = 1;
+        gbc_comboBox.insets = new Insets(0, 0, 0, 0);
+        gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+        gbc_comboBox.gridx = 2;
         gbc_comboBox.gridy = 0;
         getContentPane().add(comboBoxEmpresa, gbc_comboBox);
         
-
         if(listaEmpresas.size() > 0) {
             for(int i = 0; i <= listaEmpresas.size() - 1; i++) {
             	comboBoxEmpresa.addItem(listaEmpresas.get(i));
             }
         }
+
         
         //################################################################
         
@@ -106,22 +112,20 @@ public class CrearOfertaLaboral extends JInternalFrame{
         
         JLabel lblTipoPublicacion = new JLabel("Seleccionar tipo de publicacion");
         GridBagConstraints gbc_lblTipoPublicacion = new GridBagConstraints();
-        gbc_lblTipoPublicacion.anchor = GridBagConstraints.EAST;
+        gbc_lblTipoPublicacion.anchor = GridBagConstraints.WEST;
         gbc_lblTipoPublicacion.fill = GridBagConstraints.VERTICAL;
-        gbc_lblTipoPublicacion.insets = new Insets(0, 0, 5, 5);
-        gbc_lblTipoPublicacion.gridx = 0;
+        gbc_lblTipoPublicacion.insets = new Insets(0, 0, 5, 0);
+        gbc_lblTipoPublicacion.gridx = 1;
         gbc_lblTipoPublicacion.gridy = 1;
         getContentPane().add(lblTipoPublicacion, gbc_lblTipoPublicacion);
         
-        JComboBox<String> comboBoxTipoPublicacion = new JComboBox();
+        JComboBox<String> comboBoxTipoPublicacion = new JComboBox<String>();
         GridBagConstraints gbc_comboBoxTipoPublicacion = new GridBagConstraints();
         gbc_comboBoxTipoPublicacion.insets = new Insets(0, 0, 5, 0);
         gbc_comboBoxTipoPublicacion.fill = GridBagConstraints.HORIZONTAL;
-        gbc_comboBoxTipoPublicacion.gridx = 1;
+        gbc_comboBoxTipoPublicacion.gridx = 2;
         gbc_comboBoxTipoPublicacion.gridy = 1;
         getContentPane().add(comboBoxTipoPublicacion , gbc_comboBoxTipoPublicacion);
-        
-
         if (listaTipos.size() > 0) {
             for(int i = 0; i <= listaTipos.size() - 1; i++) {
             	comboBoxTipoPublicacion.addItem(listaTipos.get(i));
@@ -129,14 +133,13 @@ public class CrearOfertaLaboral extends JInternalFrame{
         } else {
 
         }
-        
 
       //######################### Fila 3 #############################
         JLabel lblNewLabel = new JLabel("Nombre de la oferta");
         GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-        gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
-        gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
-        gbc_lblNewLabel.gridx = 0;
+        gbc_lblNewLabel.anchor = GridBagConstraints.WEST;
+        gbc_lblNewLabel.insets = new Insets(0, 0, 5, 0);
+        gbc_lblNewLabel.gridx = 1;
         gbc_lblNewLabel.gridy = 2;
         getContentPane().add(lblNewLabel, gbc_lblNewLabel);
         
@@ -144,44 +147,43 @@ public class CrearOfertaLaboral extends JInternalFrame{
         GridBagConstraints gbc_textField = new GridBagConstraints();
         gbc_textField.insets = new Insets(0, 0, 5, 0);
         gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-        gbc_textField.gridx = 1;
+        gbc_textField.gridx = 2;
         gbc_textField.gridy = 2;
         getContentPane().add(textNombreOferta, gbc_textField);
         textNombreOferta.setColumns(10);
-        
+		
       //######################### Fila 4 #############################
-        JLabel lblNewLabel_1 = new JLabel("Descripcion");
-        GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
-        gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
-        gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
-        gbc_lblNewLabel_1.gridx = 0;
-        gbc_lblNewLabel_1.gridy = 3;
-        getContentPane().add(lblNewLabel_1, gbc_lblNewLabel_1);
+		JLabel lblNewLabel_1 = new JLabel("Descripcion");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 1;
+		gbc_lblNewLabel_1.gridy = 3;
+		getContentPane().add(lblNewLabel_1, gbc_lblNewLabel_1);
         
         
-        textDescripcion = new JTextField();
-        GridBagConstraints gbc_textField_1 = new GridBagConstraints();
-        gbc_textField_1.insets = new Insets(0, 0, 5, 0);
-        gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
-        gbc_textField_1.gridx = 1;
-        gbc_textField_1.gridy = 3;
-        getContentPane().add(textDescripcion, gbc_textField_1);
-        textDescripcion.setColumns(10);
+		JTextArea textDescripcion = new JTextArea();
+		textDescripcion.setLineWrap(true);		
+		GridBagConstraints gbc_textArea = new GridBagConstraints();
+		gbc_textArea.insets = new Insets(0, 0, 5, 5);
+		gbc_textArea.gridx = 2;
+		gbc_textArea.gridy = 3;
+		getContentPane().add(textDescripcion, gbc_textArea);
         
       //######################### Fila 5 #############################
         JLabel lblNewLabel_2 = new JLabel("Horario");
         GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
-        gbc_lblNewLabel_2.anchor = GridBagConstraints.EAST;
+        gbc_lblNewLabel_2.anchor = GridBagConstraints.WEST;
         gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
-        gbc_lblNewLabel_2.gridx = 0;
+        gbc_lblNewLabel_2.gridx = 1;
         gbc_lblNewLabel_2.gridy = 4;
         getContentPane().add(lblNewLabel_2, gbc_lblNewLabel_2);
         
         textHorario = new JTextField();
         GridBagConstraints gbc_textField_2 = new GridBagConstraints();
-        gbc_textField_2.insets = new Insets(0, 0, 5, 0);
+        gbc_textField_2.insets = new Insets(0, 0, 5, 5);
         gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
-        gbc_textField_2.gridx = 1;
+        gbc_textField_2.gridx = 2;
         gbc_textField_2.gridy = 4;
         getContentPane().add(textHorario, gbc_textField_2);
         textHorario.setColumns(10);
@@ -189,17 +191,17 @@ public class CrearOfertaLaboral extends JInternalFrame{
       //######################### Fila 6 #############################
         JLabel lblNewLabel_3 = new JLabel("Remuneracion");
         GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
-        gbc_lblNewLabel_3.anchor = GridBagConstraints.EAST;
+        gbc_lblNewLabel_3.anchor = GridBagConstraints.WEST;
         gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
-        gbc_lblNewLabel_3.gridx = 0;
+        gbc_lblNewLabel_3.gridx = 1;
         gbc_lblNewLabel_3.gridy = 5;
         getContentPane().add(lblNewLabel_3, gbc_lblNewLabel_3);
         
         textRemuneracion = new JTextField();
         GridBagConstraints gbc_textField_3 = new GridBagConstraints();
-        gbc_textField_3.insets = new Insets(0, 0, 5, 0);
+        gbc_textField_3.insets = new Insets(0, 0, 5, 5);
         gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
-        gbc_textField_3.gridx = 1;
+        gbc_textField_3.gridx = 2;
         gbc_textField_3.gridy = 5;
         getContentPane().add(textRemuneracion, gbc_textField_3);
         textRemuneracion.setColumns(10);
@@ -207,69 +209,70 @@ public class CrearOfertaLaboral extends JInternalFrame{
       //######################### Fila 7 #############################
         JLabel lblNewLabel_4 = new JLabel("Ciudad");
         GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
-        gbc_lblNewLabel_4.anchor = GridBagConstraints.EAST;
+        gbc_lblNewLabel_4.anchor = GridBagConstraints.WEST;
         gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 5);
-        gbc_lblNewLabel_4.gridx = 0;
+        gbc_lblNewLabel_4.gridx = 1;
         gbc_lblNewLabel_4.gridy = 6;
         getContentPane().add(lblNewLabel_4, gbc_lblNewLabel_4);
         
         textCiudad = new JTextField();
         GridBagConstraints gbc_textField_4 = new GridBagConstraints();
-        gbc_textField_4.insets = new Insets(0, 0, 5, 0);
+        gbc_textField_4.insets = new Insets(0, 0, 5, 5);
         gbc_textField_4.fill = GridBagConstraints.HORIZONTAL;
-        gbc_textField_4.gridx = 1;
+        gbc_textField_4.gridx = 2;
         gbc_textField_4.gridy = 6;
         getContentPane().add(textCiudad, gbc_textField_4);
         textCiudad.setColumns(10);
         
         JLabel lblNewLabel_5 = new JLabel("Departamento");
         GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
-        gbc_lblNewLabel_5.anchor = GridBagConstraints.EAST;
+        gbc_lblNewLabel_5.anchor = GridBagConstraints.WEST;
         gbc_lblNewLabel_5.insets = new Insets(0, 0, 5, 5);
-        gbc_lblNewLabel_5.gridx = 0;
+        gbc_lblNewLabel_5.gridx = 1;
         gbc_lblNewLabel_5.gridy = 7;
         getContentPane().add(lblNewLabel_5, gbc_lblNewLabel_5);
         
         textDepartamento = new JTextField();
         GridBagConstraints gbc_textField1 = new GridBagConstraints();
-        gbc_textField1.insets = new Insets(0, 0, 5, 0);
+        gbc_textField1.insets = new Insets(0, 0, 5, 5);
         gbc_textField1.fill = GridBagConstraints.HORIZONTAL;
-        gbc_textField1.gridx = 1;
+        gbc_textField1.gridx = 2;
         gbc_textField1.gridy = 7;
         getContentPane().add(textDepartamento, gbc_textField1);
         textDepartamento.setColumns(10);
         
+        List<String> listKeywords = ctrlOferta.listarKeywords();
+        
         JLabel lblNewLabel_6 = new JLabel("Fecha");
         GridBagConstraints gbc_lblNewLabel_6 = new GridBagConstraints();
-        gbc_lblNewLabel_6.anchor = GridBagConstraints.EAST;
+        gbc_lblNewLabel_6.anchor = GridBagConstraints.WEST;
         gbc_lblNewLabel_6.insets = new Insets(0, 0, 5, 5);
-        gbc_lblNewLabel_6.gridx = 0;
+        gbc_lblNewLabel_6.gridx = 1;
         gbc_lblNewLabel_6.gridy = 8;
         getContentPane().add(lblNewLabel_6, gbc_lblNewLabel_6);
         
         textFecha = new JTextField();
         GridBagConstraints gbc_textField_5 = new GridBagConstraints();
-        gbc_textField_5.insets = new Insets(0, 0, 5, 0);
+        gbc_textField_5.insets = new Insets(0, 0, 5, 5);
         gbc_textField_5.fill = GridBagConstraints.HORIZONTAL;
-        gbc_textField_5.gridx = 1;
+        gbc_textField_5.gridx = 2;
         gbc_textField_5.gridy = 8;
         getContentPane().add(textFecha, gbc_textField_5);
         textFecha.setColumns(10);
-        
-        List<String> listKeywords = ctrlOferta.listarKeywords();
+
         JLabel lblNewLabel_7 = new JLabel("Keywords");
         GridBagConstraints gbc_lblNewLabel_7 = new GridBagConstraints();
-        gbc_lblNewLabel_7.anchor = GridBagConstraints.EAST;
+        gbc_lblNewLabel_7.anchor = GridBagConstraints.WEST;
         gbc_lblNewLabel_7.insets = new Insets(0, 0, 5, 5);
-        gbc_lblNewLabel_7.gridx = 0;
+        gbc_lblNewLabel_7.gridx = 1;
         gbc_lblNewLabel_7.gridy = 9;
         getContentPane().add(lblNewLabel_7, gbc_lblNewLabel_7);
         
         JComboBox<String> comboKeywords = new JComboBox();
         GridBagConstraints gbc_comboBox_1 = new GridBagConstraints();
-        gbc_comboBox_1.insets = new Insets(0, 0, 5, 0);
+        gbc_comboBox_1.insets = new Insets(0, 0, 5, 5);
         gbc_comboBox_1.fill = GridBagConstraints.HORIZONTAL;
-        gbc_comboBox_1.gridx = 1;
+        gbc_comboBox_1.gridx = 2;
         gbc_comboBox_1.gridy = 9;
         getContentPane().add(comboKeywords, gbc_comboBox_1);
         if(listKeywords.size() > 0) {
@@ -278,19 +281,24 @@ public class CrearOfertaLaboral extends JInternalFrame{
         	}
         }
         
+        JButton btnNewButton = new JButton("Agregar");
+        GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+        gbc_btnNewButton.insets = new Insets(10, 10, 10, 10);
+        gbc_btnNewButton.gridx = 2;
+        gbc_btnNewButton.gridy = 10;
+        getContentPane().add(btnNewButton, gbc_btnNewButton);
+        gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
+        gbc_btnNewButton.anchor = GridBagConstraints.WEST;
+        gbc_btnNewButton.gridx = 1;
+        gbc_btnNewButton.gridy = 11;
+        
         
         JButton btnAceptar = new JButton("Aceptar");
         GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
-        gbc_btnNewButton_1.insets = new Insets(0, 0, 0, 5);
-        gbc_btnNewButton_1.gridx = 0;
-        gbc_btnNewButton_1.gridy = 10;
+        gbc_btnNewButton_1.insets = new Insets(0, 0, 5, 5);
+        gbc_btnNewButton_1.gridx = 1;
+        gbc_btnNewButton_1.gridy = 11;
         getContentPane().add(btnAceptar, gbc_btnNewButton_1);
-        
-        JButton btnCancelar = new JButton("Cancelar");
-        GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-        gbc_btnNewButton.gridx = 1;
-        gbc_btnNewButton.gridy = 10;
-        getContentPane().add(btnCancelar, gbc_btnNewButton);    
         // ###############################################################
         
         
@@ -329,6 +337,13 @@ public class CrearOfertaLaboral extends JInternalFrame{
 				}
 			}
         });
+        
+        JButton btnCancelar = new JButton("Cancelar");
+        GridBagConstraints gbc_btnCancelar = new GridBagConstraints();
+        gbc_btnCancelar.insets = new Insets(0, 0, 5, 5);
+        gbc_btnCancelar.gridx = 2;
+        gbc_btnCancelar.gridy = 11;
+        getContentPane().add(btnCancelar, gbc_btnCancelar);    
         
 		btnCancelar.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
