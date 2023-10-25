5cd618e1752d46c74add1b0640dc1a171ca89a4d
joaquin.cabrera <joaquin.cabrera.pastor@fing.edu.uy>
2023-10-25 19:45:17 -0300

    wsimport

---
+++ trabajouy/src/main/java/server/Empresa.java
@@ -0,0 +1,89 @@
+
+package server;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para empresa complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>{@code
+ * <complexType name="empresa">
+ *   <complexContent>
+ *     <extension base="{http://server/}usuario">
+ *       <sequence>
+ *         <element name="descripcion" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         <element name="link" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       </sequence>
+ *     </extension>
+ *   </complexContent>
+ * </complexType>
+ * }</pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "empresa", propOrder = {
+    "descripcion",
+    "link"
+})
+public class Empresa
+    extends Usuario
+{
+
+    protected String descripcion;
+    protected String link;
+
+    /**
+     * Obtiene el valor de la propiedad descripcion.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * Define el valor de la propiedad descripcion.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setDescripcion(String value) {
+        this.descripcion = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad link.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getLink() {
+        return link;
+    }
+
+    /**
+     * Define el valor de la propiedad link.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setLink(String value) {
+        this.link = value;
+    }
+
+}
